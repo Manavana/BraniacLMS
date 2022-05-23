0639ca1e7e45c3a94d7a4bb20e150722ae95bb24
Manavana <stivrina.maria@gmail.com>
2022-05-23 04:00:41 -0700

    lesson_2 Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,32 @@
-#from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+
 
 # Create your views here.
-from django.http import HttpResponse
+"""from django.http import HttpResponse
 from django.views.generic import View
 
 
@@ -11,4 +36,4 @@ class HelloWorldView(View):
 
 
 def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}")
+    return HttpResponse(f"kwargs:<br>{kwargs}")"""
