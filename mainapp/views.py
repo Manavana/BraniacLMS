d16a7e7d33869912e5a4b2a0f68af912bdfede6f
Manavana <stivrina.maria@gmail.com>
2022-05-23 04:06:03 -0700

    lesson_2 Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -23,17 +23,3 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
-
-
-# Create your views here.
-"""from django.http import HttpResponse
-from django.views.generic import View
-
-
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Hello world!_ver2")
-
-
-def check_kwargs(request, **kwargs):
-    return HttpResponse(f"kwargs:<br>{kwargs}")"""
